b2b06c55baf1e2d6dba1746d718c90f3c3ed04d7
Admin <giabao12001@gmail.com>
2022-05-30 11:18:51 +0700

    lam toi trang 24

---
+++ DuongGiaBao_Bigschool/ViewModels/CourseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using DuongGiaBao_Bigschool.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace DuongGiaBao_Bigschool.ViewModels
+{
+    public class CourseViewModel
+    {
+        [Required]
+        public string Place { get; set; }
+        [Required]
+        [FutureDate]
+        public string Date { get; set; }
+        [Required]
+        [ValidTime]
+        public string Time { get; set; }
+        [Required]
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.ParseExact(Date + " " + Time, "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+            //return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+    }
+}
